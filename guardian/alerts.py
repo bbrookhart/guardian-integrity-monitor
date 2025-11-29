30f70118fe0ebc84314d2cf6a623c3f754bf4b7d
Brian Brookhart <125527852+bbrookhart@users.noreply.github.com>
2025-11-29 13:32:14 -0600

    Update alerts.py

--- guardian/alerts.py
+++ guardian/alerts.py
@@ -1,1 +1,14 @@
-<alerts_code>
+<import time
+
+class AlertManager:
+    def __init__(self, logfile="alerts.log"):
+        self.logfile = logfile
+
+    def _write_log(self, message):
+        with open(self.logfile, "a") as f:
+            f.write(f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] {message}\n")
+
+    def notify(self, message):
+        print(f"[ALERT] {message}")
+        self._write_log(message)
+>
