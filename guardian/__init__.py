73d435ab7887fe3891da1d192b5123c04d804eb9
Brian Brookhart <125527852+bbrookhart@users.noreply.github.com>
2025-11-29 13:31:29 -0600

    Update __init__.py

---
+++ guardian/__init__.py
@@ -0,0 +1,8 @@
+"""
+Guardian Integrity Monitor package initialization.
+Provides defensive file-integrity monitoring components.
+"""
+
+from .integrity_core import IntegrityCore
+from .monitor import IntegrityMonitor
+from .alerts import AlertManager
